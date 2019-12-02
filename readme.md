51dd4a5a8544c691ff1e3c0b503f51973a9ea52a
Sergey Kravchenko <sergey@craftworks.ru>
2019-12-02 12:07:52 +0300

    configured eslint and TravisCI

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # brain-games JS
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/58f910ad3840209d0d0b/maintainability)](https://codeclimate.com/github/sergey-ag/frontend-project-lvl1/maintainability)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const welcome = () => {
+export default () => {
   console.log('Welcome to the Brain Games!\n');
 
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-}
+};
